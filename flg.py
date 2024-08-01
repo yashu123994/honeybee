fd2df1a510988bd9293e013f345daea87204017b
yashu123994 <aishwaryamnti@gmail.com>
2024-08-01 11:34:46 +0500

    hfdjh

---
+++ flg.py
@@ -0,0 +1,6 @@
+import pandas as p
+d=p.DataFrame([[2,5,6],[4,6,3],[5,7,8]],columns=["maths","java","py"])
+print(d)
+c=d.agg(['sum','min','max','count','mean','median','std','size'])
+print()
+print(c)
